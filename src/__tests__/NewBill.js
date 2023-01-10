a5b9d2c84b8270619a99aca3d49d40f01e5b73f4
Virginie <meliana.322@gmail.com>
2023-01-10 13:39:15 +0100

    Suite 1 tests NewBill

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -10,16 +10,16 @@ import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
 import mockStore from "../__mocks__/store";
 import router from "../app/Router";
-
-
-
 import { fireEvent, screen, waitFor } from "@testing-library/dom";
 
 jest.mock("../app/Store", () => mockStore);
 
 describe("Given I am connected as an employee", () => {
+  //Étant donné que je suis connecté en tant qu'employé
 	describe("When I am on NewBill Page", () => {
+    //Quand je suis sur la page NewBill
 		test("Then bill icon in vertical layout should be highlighted", async () => {
+      //Ensuite, l'icône de la facture dans la disposition verticale doit être mise en surbrillance
 			Object.defineProperty(window, "localStorage", { value: localStorageMock });
 			window.localStorage.setItem(
 				"user",
@@ -38,7 +38,9 @@ describe("Given I am connected as an employee", () => {
 		});
 	});
 	describe("When I am on NewBill Page and I select a image", () => {
-		test("then the image is uploaded if the right extention is choose", () => {
+    //Quand je suis sur NewBill Page et que je sélectionne une image
+		test("Then the image is uploaded if the right extention is choose", () => {
+      //Alors l'image est téléchargée si la bonne extension est choisie
 			Object.defineProperty(window, "localStorage", { value: localStorageMock });
 			window.localStorage.setItem(
 				"user",
@@ -72,7 +74,45 @@ describe("Given I am connected as an employee", () => {
 			expect(uploadFile.files[0].type).toBe("image/png");
 		});
 
-		test("then if I upload a file with wrong extention , a alert appear and the file is not uploaded ", () => {
+		test("Then if I upload a file with wrong extention , a alert appear and the file is not uploaded ", () => {
+      //Puis si je télécharge un fichier avec une mauvaise extension, une alerte apparaît et le fichier n'est pas téléchargé
+			jest.spyOn(window, "alert").mockImplementation(() => {});
+
+			Object.defineProperty(window, "localStorage", { value: localStorageMock });
+			window.localStorage.setItem(
+				"user",
+				JSON.stringify({
+					type: "Employee",
+				}),
+			);
+			const html = NewBillUI();
+			document.body.innerHTML = html;
+			const onNavigate = (pathname) => {
+				document.body.innerHTML = ROUTES({ pathname });
+			};
+			const handleChangeFile = jest.fn((e) =>
+				new NewBill({
+					document,
+					onNavigate,
+					store: null,
+					localStorage: window.localStorage,
+				}).handleChangeFile(e),
+			);
+			const uploadFile = screen.getByTestId("file");
+			uploadFile.addEventListener("change", handleChangeFile);
+
+			fireEvent.change(uploadFile, {
+				target: {
+					files: [new File(["image"], "fichier.pdf", { type: "application/pdf" })],
+				},
+			});
+
+			expect(handleChangeFile).toHaveBeenCalled();
+			expect(window.alert).toBeCalled();
+		});
+
+    test("Then if I upload a file with wrong extention , a alert appear and the file is not uploaded ", () => {
+      //Ensuite, si je télécharge un fichier avec une mauvaise extension, une alerte apparaît et le fichier n'est pas téléchargé
 			jest.spyOn(window, "alert").mockImplementation(() => {});
 
 			Object.defineProperty(window, "localStorage", { value: localStorageMock });
@@ -108,4 +148,5 @@ describe("Given I am connected as an employee", () => {
 			expect(window.alert).toBeCalled();
 		});
 	});
+
 })
